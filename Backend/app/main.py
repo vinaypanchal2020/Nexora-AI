1c3f85dcfc2f54fe52e4e203fbaeb42869f73f13
vinaypanchal2020 <80150729+vinaypanchal2020@users.noreply.github.com>
2026-08-13 22:37:02 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -10,10 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
+        "https://nexora-ai-mocha-beta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
